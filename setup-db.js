d92015f2018d33fe076b5d43cc1bd612cb1bce91
furtuna06 <hagezomfurtuna@gmail.com>
2026-04-18 00:19:42 +0300

    fixing final issues

--- setup-db.js
+++ setup-db.js
@@ -6,7 +6,7 @@ import path from 'path';
 async function setupDatabase() {
   const connection = await mysql.createConnection({
     host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT) ||  15695,
+    port: Number(process.env.DB_PORT),
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
